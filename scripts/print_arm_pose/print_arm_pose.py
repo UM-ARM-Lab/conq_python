2b3cb1ecae226b20ac382d06fe2776fe2b5460c4
arthurlovekin <arthurlovekin@gmail.com>
2023-11-18 18:59:11 -0500

    print_arm_pose works (not great)

--- scripts/print_arm_pose/print_arm_pose.py
+++ scripts/print_arm_pose/print_arm_pose.py
@@ -23,6 +23,7 @@ from bosdyn.client.robot_command import (RobotCommandBuilder, RobotCommandClient
 from bosdyn.client.robot_state import RobotStateClient
 
 import cv2
+import numpy as np
 
 # Define a function to do nothing (used for the trackbars)
 def nothing(x):
@@ -76,13 +77,14 @@ def print_arm_pose(config):
         """
         ################
 
+
         # Limits of the arm (meters) #TODO: get the actual limits
-        xmin = 2.0
-        xmax = -2.0
-        ymin = -1.0
-        ymax = 1.0
-        zmin = -1.0
-        zmax = 1.0
+        xmin = -2.0
+        xmax = 1.0
+        ymin = -0.5
+        ymax = 0.5
+        zmin = -0.5
+        zmax = 0.5
 
 
         # Move the arm to a spot in front of the robot, and open the gripper.
@@ -92,42 +94,45 @@ def print_arm_pose(config):
         y = 0
         z = 0.25
         
-        qw = 1
-        qx = 0
-        qy = 0
-        qz = 0
+        qw = 1.0
+        qx = 0.0
+        qy = 0.0
+        qz = 0.0
 
-        gripper_fraction = 0.0
+        gripper_percent = 0
 
+        # blank_image = np.zeros((512,512), np.uint8)
         cv2.namedWindow('Arm Pose', cv2.WINDOW_NORMAL)
-        cv2.createTrackbar("X", "Arm Pose", x, xmax, nothing)
-        cv2.createTrackbar("Y", "Arm Pose", y, ymax, nothing)
-        cv2.createTrackbar("Z", "Arm Pose", z, zmax, nothing)
-        cv2.createTrackbar("qw", "Arm Pose", qw, 1.0, nothing)
-        cv2.createTrackbar("qx", "Arm Pose", qx, 1.0, nothing)
-        cv2.createTrackbar("qy", "Arm Pose", qy, 1.0, nothing)
-        cv2.createTrackbar("qz", "Arm Pose", qz, 1.0, nothing)
-        cv2.createTrackbar("gripper", "Arm Pose", gripper_fraction, 1.0, nothing)
+        slider_max : int = 100
+        cv2.createTrackbar("X", "Arm Pose", int((x-xmin)/(xmax-xmin)*100), slider_max, nothing)
+        cv2.createTrackbar("Y", "Arm Pose", int((y-ymin)/(ymax-ymin)*100), slider_max, nothing)
+        cv2.createTrackbar("Z", "Arm Pose", int((z-zmin)/(zmax-zmin)*100), slider_max, nothing)
+        # TODO: keep the norm = 1
+        # cv2.createTrackbar("qw * 100", "Arm Pose", int(qw*100), int(1.0 *100), nothing)
+        # cv2.createTrackbar("qx * 100", "Arm Pose", int(qx*100), int(1.0 *100), nothing)
+        # cv2.createTrackbar("qy * 100", "Arm Pose", int(qy*100), int(1.0 *100), nothing)
+        # cv2.createTrackbar("qz * 100", "Arm Pose", int(qz*100), int(1.0 *100), nothing)
+        cv2.createTrackbar("gripper", "Arm Pose", 0, 100, nothing)
 
         while True:
             # Get the current positions of the trackbars
-            x = cv2.getTrackbarPos("X", "Arm Pose")
-            y = cv2.getTrackbarPos("Y", "Arm Pose")
-            z = cv2.getTrackbarPos("Z", "Arm Pose")
-            qw = cv2.getTrackbarPos("qw", "Arm Pose")
-            qx = cv2.getTrackbarPos("qx", "Arm Pose")
-            qy = cv2.getTrackbarPos("qy", "Arm Pose")
-            qz = cv2.getTrackbarPos("qz", "Arm Pose")
-            gripper_fraction = cv2.getTrackbarPos("gripper", "Arm Pose")
+            x = float(cv2.getTrackbarPos("X", "Arm Pose"))/float(slider_max)*(xmax-xmin)+xmin
+            y = float(cv2.getTrackbarPos("Y", "Arm Pose") )/float(slider_max)*(ymax-ymin)+ymin
+            z = float(cv2.getTrackbarPos("Z", "Arm Pose") )/float(slider_max)*(zmax-zmin)+zmin
+            # qw = float(cv2.getTrackbarPos("qw", "Arm Pose"))/100.0
+            # qx = float(cv2.getTrackbarPos("qx", "Arm Pose"))/100.0
+            # qy = float(cv2.getTrackbarPos("qy", "Arm Pose"))/100.0
+            # qz = float(cv2.getTrackbarPos("qz", "Arm Pose"))/100.0
+            gripper_percent = float(cv2.getTrackbarPos("gripper", "Arm Pose"))
             # check the limits
             x = max(min(x, xmax), xmin)
             y = max(min(y, ymax), ymin)
             z = max(min(z, zmax), zmin)
-            qw = max(min(qw, 1.0), -1.0)
-            qx = max(min(qx, 1.0), -1.0)
-            qy = max(min(qy, 1.0), -1.0)
-            qz = max(min(qz, 1.0), -1.0)
-            gripper_fraction = max(min(gripper_fraction, 1.0), 0.0)
+            # qw = max(min(qw, 1.0), -1.0)
+            # qx = max(min(qx, 1.0), -1.0)
+            # qy = max(min(qy, 1.0), -1.0)
+            # qz = max(min(qz, 1.0), -1.0)
+            gripper_percent = max(min(gripper_percent, 100), 0)
 
             # Convert to a boston dynamics pose
             hand_ewrt_flat_body = geometry_pb2.Vec3(x=x, y=y, z=z)
@@ -149,14 +154,14 @@ def print_arm_pose(config):
                 odom_T_hand.rot.y, odom_T_hand.rot.z, ODOM_FRAME_NAME, seconds)
 
             # Make the open gripper RobotCommand
-            gripper_command = RobotCommandBuilder.claw_gripper_open_fraction_command(1.0)
+            gripper_command = RobotCommandBuilder.claw_gripper_open_fraction_command(float(gripper_percent)/100.0)
 
             # Combine the arm and gripper commands into one RobotCommand
             command = RobotCommandBuilder.build_synchro_command(gripper_command, arm_command)
 
             # Send the request
+            robot.logger.info(f"Moving arm to position: ({x:0.3f}, {y:0.3f}, {z:0.3f}), ({qw:0.3f}, {qx:0.3f}, {qy:0.3f}, {qz:0.3f}), {gripper_percent})")
             cmd_id = command_client.robot_command(command)
-            robot.logger.info('Moving arm to position 1.')
 
             # Wait until the arm arrives at the goal.
             block_until_arm_arrives_with_prints(robot, command_client, cmd_id)
@@ -165,41 +170,50 @@ def print_arm_pose(config):
             robot_state = robot_state_client.get_robot_state()
             odom_T_flat_body = get_a_tform_b(robot_state.kinematic_state.transforms_snapshot,
                                             ODOM_FRAME_NAME, GRAV_ALIGNED_BODY_FRAME_NAME)
-            print(f"Current arm pose (x,y,x,qx,qy,qz,qw,g): ({odom_T_flat_body.x}, {odom_T_flat_body.y}, {odom_T_flat_body.z}), 
-                    ({odom_T_flat_body.rot.x}, {odom_T_flat_body.rot.y}, {odom_T_flat_body.rot.z}, {odom_T_flat_body.rot.w}), {gripper_fraction}")
+            # text = f"Current body odom pose (x,y,x,qx,qy,qz,qw,g): ({odom_T_flat_body.x:0.3f}, {odom_T_flat_body.y:0.3f}, {odom_T_flat_body.z:0.3f}), \
+            #         ({odom_T_flat_body.rot.x:0.3f}, {odom_T_flat_body.rot.y:0.3f}, {odom_T_flat_body.rot.z:0.3f}, {odom_T_flat_body.rot.w:0.3f}), {gripper_percent}"
+            
+            # robot.logger.info(text)
             # time.sleep(0.1)
 
+            # if the user presses 'q', exit the program
+            if cv2.waitKey(1) & 0xFF == ord('q'):
 
-        # if the user presses 'q', exit the program
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-
-            # Power the robot off. By specifying "cut_immediately=False", a safe power off command
-            # is issued to the robot. This will attempt to sit the robot before powering off.
-            robot.power_off(cut_immediately=False, timeout_sec=20)
-            assert not robot.is_powered_on(), "Robot power off failed."
-            robot.logger.info("Robot safely powered off.")
-            return
+                # Power the robot off. By specifying "cut_immediately=False", a safe power off command
+                # is issued to the robot. This will attempt to sit the robot before powering off.
+                robot.power_off(cut_immediately=False, timeout_sec=20)
+                assert not robot.is_powered_on(), "Robot power off failed."
+                robot.logger.info("Robot safely powered off.")
+                return
 
 
-def block_until_arm_arrives_with_prints(robot, command_client, cmd_id):
+def block_until_arm_arrives_with_prints(robot, command_client, cmd_id, position_thresh=0.005, timeout_sec=5):
     """Block until the arm arrives at the goal and print the distance remaining.
         Note: a version of this function is available as a helper in robot_command
         without the prints.
     """
+    t0 = time.time()
+    print("Blocking until arm arrives at goal...")
     while True:
         feedback_resp = command_client.robot_command_feedback(cmd_id)
-        robot.logger.info(
-            'Distance to go: ' +
-            '{:.2f} meters'.format(feedback_resp.feedback.synchronized_feedback.arm_command_feedback
-                                   .arm_cartesian_feedback.measured_pos_distance_to_goal) +
-            ', {:.2f} radians'.format(
-                feedback_resp.feedback.synchronized_feedback.arm_command_feedback.
-                arm_cartesian_feedback.measured_rot_distance_to_goal))
+        # robot.logger.info(
+        #     'Distance to go: ' +
+        #     '{:.2f} meters'.format(feedback_resp.feedback.synchronized_feedback.arm_command_feedback
+        #                            .arm_cartesian_feedback.measured_pos_distance_to_goal) +
+        #     ', {:.2f} radians'.format(
+        #         feedback_resp.feedback.synchronized_feedback.arm_command_feedback.
+        #         arm_cartesian_feedback.measured_rot_distance_to_goal))
 
-        if feedback_resp.feedback.synchronized_feedback.arm_command_feedback.arm_cartesian_feedback.status == arm_command_pb2.ArmCartesianCommand.Feedback.STATUS_TRAJECTORY_COMPLETE:
-            robot.logger.info('Move complete.')
-            break
+        if (feedback_resp.feedback.synchronized_feedback.arm_command_feedback.arm_cartesian_feedback.status == arm_command_pb2.ArmCartesianCommand.Feedback.STATUS_TRAJECTORY_COMPLETE
+            or (feedback_resp.feedback.synchronized_feedback.arm_command_feedback.arm_cartesian_feedback.measured_pos_distance_to_goal < position_thresh and
+                feedback_resp.feedback.synchronized_feedback.arm_command_feedback.arm_cartesian_feedback.measured_rot_distance_to_goal < position_thresh)):
+            # robot.logger.info('Move complete.')
+            return True
         time.sleep(0.1)
+
+        if time.time() - t0 > timeout_sec:
+            robot.logger.info('Move timed out.')
+            return False
 
 
 def main(argv):
