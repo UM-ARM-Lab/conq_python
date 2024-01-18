0ebc4dcb3b88e5c05b85f4fa57689b30a45256b8
Dylan Colli <dylanfrankcolli@gmail.com>
2024-01-18 13:38:28 -0500

    Refactor replay demo path manipulation for clarity

--- scripts/data_recorder_playback_demo.py
+++ scripts/data_recorder_playback_demo.py
@@ -7,24 +7,35 @@ from conq.cameras_utils import image_to_opencv
 
 RR_TIMELINE_NAME = "stable_time"
 NANOSECONDS_TO_SECONDS = 1e-9
-ROOT_PATH = Path("/media/big_narstie/datasets/conq_hose_manipulation_raw/")
+
+
+def get_log_paths():
+    """Do all the path manipulation necessary to point to log and metadata files
+
+    This is just a convenience function to make the main function less cluttered.
+    """
+    root_path = Path("/media/big_narstie/datasets/conq_hose_manipulation_raw/")
+
+    exp_name = "conq_hose_manipulation_data_1700079751"
+    # exp_name = "conq_hose_manipulation_data_1700080718"
+    # exp_name = "conq_hose_manipulation_data_1700081021"
+    # exp_name = "conq_hose_manipulation_data_1700082856"
+    exp_path = root_path / exp_name
+
+    episode_num = 0
+    # episode_num = 6
+    # episode_num = 8
+
+    pkl_path = exp_path / "train" / f"episode_{episode_num}.pkl"
+    metadata_path = exp_path / "metadata.json"
+
+    return pkl_path, metadata_path
 
 
 def main():
     rr.init("data_recorder_playback_rerun", spawn=True)
 
-    # Do all path manipulation necessary to point to log file and metadata file.
-    # exp_name = "conq_hose_manipulation_data_1700079751"
-    # exp_name = "conq_hose_manipulation_data_1700080718"
-    # exp_name = "conq_hose_manipulation_data_1700081021"
-    exp_name = "conq_hose_manipulation_data_1700082856"
-    exp_path = ROOT_PATH / exp_name
-    # episode_num = 0
-    episode_num = 6
-    # episode_num = 8
-    pkl_path = exp_path / "train" / f"episode_{episode_num}.pkl"
-    metadata_path = exp_path / "metadata.json"
-
+    pkl_path, metadata_path = get_log_paths()
     log = ConqLog(pkl_path, metadata_path)
 
     rr.set_time_seconds(RR_TIMELINE_NAME, log.get_t_start())
@@ -43,9 +54,8 @@ def main():
         # print(timestamp)
         # use snapshot to get the transforms
 
-        # for source_name, res in packet.image_iterator(rgb_sources, depth_sources):
-        for source_name, res in packet.image_iterator():
-            timestamp_secs = 1e-9 * res.shot.acquisition_time.ToNanoseconds()
+        for source_name, res in packet.image_iterator(rgb_sources, depth_sources):
+            timestamp_secs = NANOSECONDS_TO_SECONDS * res.shot.acquisition_time.ToNanoseconds()
 
             # Sets the publication time for the rerun message.
             rr.set_time_seconds(RR_TIMELINE_NAME, timestamp_secs)
