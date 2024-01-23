c53c347f619052c74804a879415bec62774b1560
Dylan Colli <dylanfrankcolli@gmail.com>
2024-01-23 18:57:04 -0500

    Add GraphNav waypoint and edge visualization
    - Only for anchored graphs. Should probably create a base class and derived classes for anchored/non-anchored graphs since they share a lot of the same functions.

--- scripts/data_recorder_playback_map.py
+++ scripts/data_recorder_playback_map.py
@@ -12,6 +12,9 @@ from bosdyn.client.math_helpers import *
 
 from conq.navigation_lib.map import get_point_cloud_data_in_seed_frame, load_map
 
+WAYPOINT_COLOR = np.array((1.0, 1.0, 1.0), dtype=float).reshape(1, 3)
+EDGE_COLOR = WAYPOINT_COLOR.copy()
+
 
 class MplColorHelper:
     """Adapted from https://stackoverflow.com/a/26109298"""
@@ -24,6 +27,10 @@ class MplColorHelper:
 
     def get_rgb(self, val: np.ndarray):
         return self.scalarMap.to_rgba(val)
+
+
+# def visualize_waypoints_in_seed_frame(current_graph):
+#     for
 
 
 def main():
@@ -67,6 +74,59 @@ def main():
     rr.log("map_point_cloud", rr.Points3D(data, colors=pc_colors, radii=0.02))
 
     # TODO: Default to showing origin?
+
+    # Doing the Breadth-first search is for un-anchored graphs.
+    # queue = []
+    # queue.append((current_graph.waypoints[0], np.eye(4)))
+    # visited = {}
+    waypoint_cloud = []
+    # while len(queue) > 0:
+    #     curr_element = queue.pop(0)
+    #     curr_waypoint = curr_element[0]
+    #     if curr_waypoint.id in visited:
+    #         continue
+    #     visited[curr_waypoint.id] = True
+
+    #     # We now know the global pose of this waypoint, so set the pose.
+
+    # Visualize waypoints
+    # TODO: Refactor into separate function.
+    # waypoint_objects = {}
+    for waypoint in current_graph.waypoints:
+        if waypoint.id in current_anchors:
+            # waypoint_objects[waypoint.id] = create_waypoint_object(current_waypoints,
+            # current_waypoint_snapshots, waypoint.id)
+            # Just store the location in seed frame for this waypoint.
+            # waypoint_objects[waypoint.id]
+
+            seed_tform_waypoint = SE3Pose.from_proto(
+                current_anchors[waypoint.id].seed_tform_waypoint).to_matrix()
+            waypoint_pos_in_seed_frame = seed_tform_waypoint[:3, 3]
+            waypoint_cloud.append(waypoint_pos_in_seed_frame)
+
+    # Now plot the edges.
+    edge_num = 0
+    for edge in current_graph.edges:
+        if edge.id.from_waypoint in current_anchors and edge.id.to_waypoint in current_anchors:
+            seed_tform_from = SE3Pose.from_proto(
+                current_anchors[edge.id.from_waypoint].seed_tform_waypoint).to_matrix()
+            from_tform_to = SE3Pose.from_proto(edge.from_tform_to).to_matrix()
+            # Create edge object.
+            world_tform_to_wp = np.dot(seed_tform_from, from_tform_to)
+            # Make line between the current waypoint and the neighbor.
+            pos_start = seed_tform_from[:3, 3]
+            pos_end = world_tform_to_wp[:3, 3]
+            line = np.stack((pos_start, pos_end), axis=0)
+            name = f"edge_{edge_num}"
+            rr.log(name, rr.LineStrips3D(line, colors=EDGE_COLOR))
+            edge_num += 1
+
+    # TODO: Create rerun objects associated with each anchored world object.
+
+    waypoint_cloud = np.stack(waypoint_cloud, axis=0)
+    waypoint_colors = WAYPOINT_COLOR.repeat(waypoint_cloud.shape[0], 0)
+
+    rr.log("waypoints", rr.Points3D(waypoint_cloud, colors=waypoint_colors, radii=0.1))
 
 
 if __name__ == "__main__":
