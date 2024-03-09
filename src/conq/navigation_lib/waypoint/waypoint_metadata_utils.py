109a4382a7405fc101e58ec31f23d4dd053f8345
Jacob Skwirsk <skwirskj@umich.edu>
2024-03-09 17:31:33 -0500

    Added template for metadata encoder/decoder

---
+++ src/conq/navigation_lib/waypoint/waypoint_metadata_utils.py
@@ -0,0 +1,13 @@
+"""This file contains helper functions for encoding and decoding data for waypoints in JSON format.
+"""
+
+"""Takes in a JSON object containing new metadata for a waypoint, the existing waypoint metadata, and returns a string representation of the JSON object.
+"""
+def encode_waypoint_metadata():
+    pass
+
+
+"""Takes in a string representing a waypoint's metadata and returns a JSON object with the information.
+"""
+def decode_waypoint_metadata():
+    pass
