1b889e97cfdd1818c53ea752909ff275f476d662
Jacob Skwirsk <skwirskj@umich.edu>
2024-04-03 15:14:32 -0400

    Removed waypoint folder since it was not used

--- src/conq/clients.py
+++ src/conq/clients.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from bosdyn.client.graph_nav import GraphNavClient
 from bosdyn.client.image import ImageClient
 from bosdyn.client.lease import LeaseClient
 from bosdyn.client.manipulation_api_client import ManipulationApiClient
@@ -8,7 +9,6 @@ from bosdyn.client.ray_cast import RayCastClient
 from bosdyn.client.robot import Robot
 from bosdyn.client.robot_command import RobotCommandClient
 from bosdyn.client.robot_state import RobotStateClient
-from bosdyn.client.graph_nav import GraphNavClient
 from bosdyn.client.world_object import WorldObjectClient
 
 
