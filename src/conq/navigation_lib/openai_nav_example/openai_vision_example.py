e56d77f46f7bac8c79d045f9d71ce0c6d3783a02
Jacob Skwirsk <skwirskj@umich.edu>
2024-04-16 11:27:52 -0400

    Made vision example use a JSON for the input and updated the README

--- src/conq/navigation_lib/openai_nav_example/openai_vision_example.py
+++ src/conq/navigation_lib/openai_nav_example/openai_vision_example.py
@@ -1,15 +1,38 @@
+import argparse
+import json
+import sys
+
 from conq.navigation_lib.openai_nav.openai_nav_client import OpenAINavClient
 
-if __name__ == "__main__":
+# Main function with argument parsing that takes in a relative path to a JSON file and then saves the prompt and image URL array into variables
+# Then, it creates an OpenAINavClient object and calls the generate_label_for_image function with the prompt and image URL
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='OpenAI Vision API Example')
+    parser.add_argument('--json-filepath', type=str, help='Relative path to JSON file with prompt and image URL', required=True)
+    args = parser.parse_args(argv)
+
+    with open(args.json_filepath, 'r') as file:
+        data = file.read()
+
+    data = json.loads(data)
+    prompt = data['user_prompt']
+    image_urls = data['image_inputs']
+
     client = OpenAINavClient(locations=[])
 
-    # Creating while loop to debug with
     while True:
-        user_prompt = input("Enter prompt, (q) to quit: ")
+        user_prompt = input("Enter prompt, (j) for input in JSON file, (q) to quit, or another string to be used as the prompt: ")
         if user_prompt == "q":
             break
+        elif user_prompt == "j":
+            for image_url in image_urls:
+                print(f"Processing Image URL: {image_url}")
+                client.generate_label_for_image(prompt=prompt, image_url=image_url)
         else:
-            print("First image processing...")
-            client.generate_label_for_image(prompt=user_prompt, image_url="https://www.fromhousetohome.com/garden/wp-content/uploads/sites/2/2022/05/garden-tool-storage-ideas-3.jpg")
-            print("Second image processing...")
-            client.generate_label_for_image(prompt=user_prompt, image_url="https://img.hobbyfarms.com/wp-content/uploads/2009/02/18104206/harvest-tools_624.jpg")
+            for image_url in image_urls:
+                print(f"Processing Image URL: {image_url}")
+                client.generate_label_for_image(prompt=user_prompt, image_url=image_url)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
