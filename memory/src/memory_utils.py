4ab098a380f51a5b9ea0fae26b8d57be026c5ffa
jbrhm <jrabraham1904@gmail.com>
2024-05-23 14:53:40 -0400

    Refactoring

--- memory/src/memory_utils.py
+++ memory/src/memory_utils.py
@@ -64,41 +64,4 @@ class Memory:
         for source in SOURCES:
             self._store_lens(source)
 
-# Testing for the functions
-if __name__ == "__main__":
-    # Create an instance of the boston dynamics sdk with a name
-    # I think these functions are included in everything that is in the bosdyn.client package
-    sdk = bosdyn.client.create_standard_sdk('semantic_memory') 
-    robot = sdk.create_robot('192.168.80.3')
-
-    # This is the function that uses the username and password env vars to verify that we should have access to spot
-    bosdyn.client.util.authenticate(robot)
-
-    # This is a homebrewed function ensures a given robot is not currently e-stopped
-    verify_estop(robot)
-
-    image_client = robot.ensure_client(ImageClient.default_service_name)
-    lease_client = robot.ensure_client(LeaseClient.default_service_name)
-    command_client = robot.ensure_client(RobotCommandClient.default_service_name)
-
-    # I am fairly confident that this with statement's primary goal is to ensure that which ever device is currently leasing the robot is available through this entire function
-    with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
-        
-        # Powering on sequence
-        robot.logger.info('Powering on robot... This may take several seconds.')
-        robot.power_on() # this function will ensure that the robot is powered on within the default time period of 20 seconds
-        assert robot.is_powered_on(), "Robot failed to power on." # This is an assert statement that will ensure that the robot is properly powered on, otherwise it will raise an exception
-        robot.logger.info('The robot has powered on.')
-
-        # Create a client object which is just a "struct" of all of the other clients used on the robot packaged nicely
-        
-        robot.logger.info('Commanding the robot to stand')
-        stand(robot, command_client)
-
-        # This should return a numpy array which contains the image from the 'right_fisheye_image' camera on spot
-        memory = Memory(image_client)
-        memory._dump_json()
-        memory._load_json()
-        memory._dump_json()
-        memory.observe_surroundings()
 
