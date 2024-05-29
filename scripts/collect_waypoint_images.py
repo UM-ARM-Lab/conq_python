a3feb9c6d9ec3057cab58c3bc94e853c318506f5
adi-balaji <advaithb@umich.edu>
2024-05-29 17:43:56 -0400

    script for taking pictures for full map

---
+++ scripts/collect_waypoint_images.py
@@ -0,0 +1,21 @@
+# export PYTHONPATH="${PYTHONPATH}:/Users/adibalaji/Desktop/agrobots/conq_python/src"
+
+import bosdyn.client.util
+import bosdyn.client.lease
+from bosdyn.client.lease import LeaseClient
+import time
+
+from conq.navigation.graph_nav.waypoint_photographer import WaypointPhotographer
+
+# Setup and authenticate the robot.
+sdk = bosdyn.client.create_standard_sdk('WaypointPhotographerClient')
+robot = sdk.create_robot('192.168.80.3')
+bosdyn.client.util.authenticate(robot) 
+
+lease_client = robot.ensure_client(LeaseClient.default_service_name)
+
+lease_client.take()
+
+with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
+    wp = WaypointPhotographer(robot)
+    wp.take_photos_of_full_map()
